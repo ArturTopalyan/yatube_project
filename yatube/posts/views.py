7c3a0132e2895aae5313229beee6bd52bf748477
Артур Топалян <topabro@yandex.ru>
2021-11-20 22:17:25 +0300

    HTML-шаблоны подключение

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,37 @@
-#from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    # Адрес шаблона сохраним в переменную, это не обязательно, но удобно
+    template = 'posts/index.html'
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    title = 'Последние обновления на сайте'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': "Это главная страница проекта Yatube",
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
 # Страница со списком групп сообщества
-def group_posts(request, slug):
-    return HttpResponse(f'Cтраницы, на которых будут посты, отфильтрованные по группам {slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+
+    title = 'Группы проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': "Здесь будет информация о группах проекта Yatube",
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 # Create your views here.
