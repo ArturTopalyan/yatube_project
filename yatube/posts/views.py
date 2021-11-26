35c5bb8ce7962869151c0e1bb82f21e2f91136b8
Артур Топалян <topabro@yandex.ru>
2021-11-26 16:01:58 +0300

    правки после ревью1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,10 +4,8 @@ from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
-    text = "Это главная страница проекта Yatube"
+    posts = Post.objects.select_related('group')[:10]
     context = {
-        'text': text,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
