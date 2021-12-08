1e0095e0aecd89e151eea15fdf6bf3df222102ad
Артур Топалян <topabro@yandex.ru>
2021-12-08 19:02:58 +0300

    Доработка проекта с футером

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,29 @@
+from django.core.paginator import Paginator
+
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.select_related('group')[:10]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
+        'page_obj': page_obj,
         'group': group,
-        'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
